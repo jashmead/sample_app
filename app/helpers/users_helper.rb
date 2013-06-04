2f00cd9dc753f7647c9fdfa279c6fde79b5ed17e
John Ashmead <john.ashmead@ashmeadsoftware.com>
2013-06-04 17:58:36 -0400

    tutorial 7.1 gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
+	# not showing correctly in Firefox (see only alt text) but seems ok in Chrome -- continue!
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
