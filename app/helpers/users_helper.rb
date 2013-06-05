5e43fe8d4f838884d09464fdea9e30102357515d
John Ashmead <john.ashmead@ashmeadsoftware.com>
2013-06-05 11:41:39 -0400

    fixed gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,8 +3,10 @@ module UsersHelper
   # Returns the Gravatar (http://gravatar.com/) for the given user.
 	# not showing correctly in Firefox (see only alt text) but seems ok in Chrome -- continue!
   def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+		if user and user.email
+    	gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    	gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    	image_tag(gravatar_url, alt: user.name, class: "gravatar")
+		end
   end
 end
