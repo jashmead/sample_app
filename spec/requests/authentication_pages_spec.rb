aac2183685abcde8f44a4373e4bee0bffd4a8457
John Ashmead <john.ashmead@ashmeadsoftware.com>
2013-06-06 16:48:49 -0400

    tutorial 10.3.1

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -97,6 +97,20 @@ describe "Authentication" do
 
           end
 
+					# how is it that 'post' & 'delete' are actually working?, perhaps there is just little to do?
+          describe "in the Microposts controller" do
+    
+            describe "submitting to the create action" do
+              before { post microposts_path }
+              specify { expect(response).to redirect_to(signin_path) }
+            end
+    
+            describe "submitting to the destroy action" do
+              before { delete micropost_path(FactoryGirl.create(:micropost)) }
+              specify { expect(response).to redirect_to(signin_path) }
+            end
+          end
+
         end
 
         describe "as wrong user" do
