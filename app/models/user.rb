18616d0cfc91bb82538c5491c610c968f38eee11
John Ashmead <john.ashmead@ashmeadsoftware.com>
2013-06-06 21:58:18 -0400

    tutorial 11.1.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
 	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 	# no 'has_many :relationships, foreign_key: "followed_id", dependent: :destroy' ?
+	has_many :followed_users, through: :relationships, source: :followed
 
 	before_save { self.email = email.downcase }
 	before_save :create_remember_token 
@@ -16,9 +17,21 @@ class User < ActiveRecord::Base
 	validates :password_confirmation, presence: true
 	validates :password, length: { minimum: 6 }
 
- def feed
+ 	def feed
     # This is preliminary. See "Following users" for the full implementation.
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
   end
 
   private
